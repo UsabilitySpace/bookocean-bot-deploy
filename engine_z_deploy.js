ead38628a3ed7ca9232f13f99e7abc7fc1371679
Usability.Space <info@usability.space>
2022-01-11 07:25:56 +0700

    save Tue Jan 11 07:25:56 +07 2022

--- engine_z_deploy.js
+++ engine_z_deploy.js
@@ -75,11 +75,11 @@ for (let BOT_N in Object.keys(a)) {
 
     // ${JSON.stringify(bots_to_export[BOT_N])}
     let BOT_N_DATA_1_ECOSYSTEM = `module.exports = {
-        apps : [
-            script : ${bots_to_export[BOT_N].script},
-            name   : ${bots_to_export[BOT_N].name},
-            args   : ${bots_to_export[BOT_N].args}
-        ]
+        apps : [{ 
+            script : "${bots_to_export[BOT_N].script}",
+            name   : "${bots_to_export[BOT_N].name}",
+            args   : "${bots_to_export[BOT_N].args}"
+        }]
     }`;
 
     let BOT_N_PATH_1_ECOSYSTEM = `${CONFIG_BOT_RUN}/${BOT_N}_${BOT_N_NAME}_ECOSYSTEM.js`;
