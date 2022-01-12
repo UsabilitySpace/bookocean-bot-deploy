5cb9eb3ca7acce0d09196454e4b9a2ced1bcf685
Usability.Space <info@usability.space>
2022-01-12 23:44:21 +0700

    save Wed Jan 12 23:44:21 +07 2022

--- engine_z_deploy.js
+++ engine_z_deploy.js
@@ -47,7 +47,7 @@ const config_of_this_IP = {
 // EXPORTS
 // ════════════════════════════════════════════════════════
 
-const CONFIG_BOT_RUN = './engine_run';
+const CONFIG_BOT_RUN = '/var/www/www-root/data/nodejs/server/usability-space-bot-engine/deploy/engine_run';
 
 let files = fs.readdirSync(CONFIG_BOT_RUN);
 for (const file of files) {
